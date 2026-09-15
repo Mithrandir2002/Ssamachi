b57fa7c865fcb146a0a551aaa7d747641be441b6
minhson <snightluve@gmail.com>
2026-09-15 13:36:16 +0700

    t6

--- core-service/src/main/java/com/earthquake/core/repository/EarthquakeRepository.java
+++ core-service/src/main/java/com/earthquake/core/repository/EarthquakeRepository.java
@@ -2,6 +2,8 @@ package com.earthquake.core.repository;
 
 import com.earthquake.core.domain.Earthquake;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,12 @@ public interface EarthquakeRepository extends JpaRepository<Earthquake, String> 
     // TODO: derived/native query methods for filtering by date range, magnitude range
     // and spatial "nearby" search will be added when the query logic is implemented.
     Optional<Earthquake> findEarthquakeById(String id);
+
+    @Modifying
+    @Query(value = """
+    UPDATE earthquakes
+    SET location = geography::Point(:latitude, :longitude, 4326)
+    WHERE id = :id
+    """, nativeQuery = true)
+    void updateLocation(String id, double latitude, double longitude);
 }
